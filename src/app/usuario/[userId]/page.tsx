760a1b8001b1df8a23ebd39f5ea74f6bdcd0772a
Erick0105 <erickspot01@gmail.com>
2024-11-16 18:21:27 -0300

    vendo no que da

--- src/app/usuario/[userId]/page.tsx
+++ src/app/usuario/[userId]/page.tsx
@@ -2,23 +2,31 @@
 
 import { TipoUsuario } from "@/types"
 import { useRouter } from "next/router"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Usuario() {
-    
-    const router = useRouter();
-    const [user] = useState<TipoUsuario>({
-        id: Number(router.query),
-        nome: "",
-        email: "",
-        senha: "",
-    })
+  const router = useRouter();
+  const [user, setUser] = useState<TipoUsuario>({
+    id: 0,
+    nome: "",
+    email: "",
+    senha: "",
+  });
 
-    
+  // Atualizando o estado assim que o router.query estiver disponível
+  useEffect(() => {
+    if (router.query.userId) {
+      setUser(prevState => ({
+        ...prevState,
+        id: Number(router.query.userId), // Convertendo para número
+      }));
+    }
+  }, [router.query]);
+
   return (
     <main>
-        <h1>Página de Usuario</h1>
-        <h4>ID - {user.id}</h4>
+      <h1>Página de Usuário</h1>
+      <h4>ID - {user.id}</h4>
     </main>
-  )
+  );
 }
