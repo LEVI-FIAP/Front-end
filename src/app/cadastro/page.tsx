ef9488a1c3242d9108095add0dd7bba28ea7b5df
Erick0105 <erickspot01@gmail.com>
2024-11-16 17:07:29 -0300

    resetando o projeto fiquei o dia todo pra no final resetar essa bagaça

--- src/app/cadastro/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Cadastro() {
-  return (
-    <main>
-        <h1>Cadastro</h1>
-    </main>
-  )
-}
--- src/app/sobre/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Sobre() {
-  return (
-    <main>
-        <h1>Quem Somos!</h1>
-    </main>
-  )
-}
