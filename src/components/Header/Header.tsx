a0bf4d28defca0dd527fbd66d9e60de8204067d3
Erick0105 <erickspot01@gmail.com>
2024-11-17 16:08:12 -0300

    esqueci de comentar as coisas

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,31 @@
-"use client"
+// "use client"
 
-import React, { useState } from 'react'
-import Nav from '../Nav/Nav'
+// import React, { useState } from 'react'
 
 export default function Header() {
 
-    const [navStatus, setNavStatus] = useState(false);
-    const [opcoesStatus, setOpcoesStatus] = useState(false);
+    // const [navStatus, setNavStatus] = useState(false);
+    // const [opcoesStatus, setOpcoesStatus] = useState(false);
 
-    const mudarNav = () => {
-        setNavStatus(false);
-        setOpcoesStatus(false);
-        if(!navStatus){
-            setNavStatus(true)
-        }
-    };
+    // const mudarNav = () => {
+    //     setNavStatus(false);
+    //     setOpcoesStatus(false);
+    //     if(!navStatus){
+    //         setNavStatus(true)
+    //     }
+    // };
 
-    const mudarOpcoes = () => {
-        setOpcoesStatus(false);
-        setNavStatus(false);
-        if(!opcoesStatus){
-            setOpcoesStatus(true)
-        }
-    };
+    // const mudarOpcoes = () => {
+    //     setOpcoesStatus(false);
+    //     setNavStatus(false);
+    //     if(!opcoesStatus){
+    //         setOpcoesStatus(true)
+    //     }
+    // };
 
   return (
     <header>
+        <h1>Cabecalho</h1>
     </header>
   )
 }
