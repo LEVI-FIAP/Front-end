9093490e96076408d993875d2bd587261f0a2b30
Erick0105 <erickspot01@gmail.com>
2024-11-17 16:05:59 -0300

    lendo documentação pra ver no que da

--- src/app/usuario/[userId]/page.tsx
+++ src/app/usuario/[userId]/page.tsx
@@ -1,27 +1,8 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
-
-interface UsuarioProps {
-  userId: string;
-}
-
-// Corrigindo `getStaticPaths` com TypeScript
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = [{ params: { userId: '1' } }, { params: { userId: '2' } }];
-  return { paths, fallback: false };
-};
-
-// Corrigindo `getStaticProps` com TypeScript
-export const getStaticProps: GetStaticProps<UsuarioProps> = async ({ params }) => {
-  const userId = params?.userId as string;
-  return { props: { userId } };
-};
-
-const Usuario: React.FC<UsuarioProps> = ({ userId }) => {
-  return (
-    <main>
-      <h1>Usuário {userId}</h1>
-    </main>
-  );
-};
-
-export default Usuario;
+export default async function Usuario({
+    params,
+  }: {
+    params: Promise<{ userId: string }>
+  }) {
+    const idUser = (await params).userId
+    return <div>My Post: {idUser}</div>
+  }
