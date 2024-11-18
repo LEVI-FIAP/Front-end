65eb16fb25bfea2d459079a75bb970057e5ab34b
Erick0105 <erickspot01@gmail.com>
2024-11-17 22:43:49 -0300

    notFound e loading criados

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='notFound'>
+        <h1> ERROR 404 - NotFound</h1>
+        <h2>This page could not be found.</h2>
+    </div>
+  )
+}
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-        <h2>&copy; Copyright</h2>
+        <h2>&copy; Copyright </h2>
     </footer>
   )
 }
