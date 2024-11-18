53bdf1fcfba044304ca26c43a80550938ca78831
LuizHNR <luizhneri12@gmail.com>
2024-11-18 15:32:39 -0300

    botão rotate

--- src/components/Carrossel/Carrosel.tsx
+++ src/components/Carrossel/Carrosel.tsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 export default function Carrossel () {
     
   return (
-    <Swiper spaceBetween={50} slidesPerView={3} loop={true}>
+    <Swiper spaceBetween={50} slidesPerView={3} loop={true} className="flex">
       <SwiperSlide>Slide 1</SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
