5f38a54be156de7b74ca1a7dc731f14679f36f77
LuizHNR <luizhneri12@gmail.com>
2024-11-19 12:50:24 -0300

    estilizado

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,20 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        telefone: "#E41B69",
+        faq: "#E8E8E8",
+      },
+      screens: {
+        'phone': '200px',
+      },
+      gap: {
+        '100': '450px',
+      },
+      spacing: {
+        '50': '50%',
+        '101': '100%',
+        '105': '400px',
+        '110': '569px',
       },
     },
   },
